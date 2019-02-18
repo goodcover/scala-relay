5a50610a86e5672633c2f1d0148fef75711e8278
Dan Di Spaltro <dan.dispaltro@gmail.com>
2019-02-18 11:13:17 -0800

    v0.1.5

--- relay-macro/src/main/scala/relay/gql/TaggedNode.scala
+++ relay-macro/src/main/scala/relay/gql/TaggedNode.scala
@@ -4,7 +4,7 @@ import scala.language.implicitConversions
 import scala.scalajs.js
 
 trait GenericGraphQLTaggedNode {
-  val query: TaggedNode
+  def query: TaggedNode
 }
 
 object GenericGraphQLTaggedNode {
@@ -43,20 +43,5 @@ trait ConcreteFragment extends TaggedNode
 @js.native
 trait ConcreteBatch extends TaggedNode
 
-object TaggedNode {
-  type Func = js.Function0[TaggedNode]
-
-  implicit def gqlBase2func(g: TaggedNode): Func = () => g
-}
-
-object ConcreteFragment {
-  type Func = js.Function0[ConcreteFragment]
-
-  implicit def gqlBase2func(g: ConcreteFragment): Func = () => g
-}
-
-object ConcreteBatch {
-  type Func = js.Function0[ConcreteBatch]
-
-  implicit def gqlBase2func(g: ConcreteBatch): Func = () => g
-}
+@js.native
+trait ReaderFragment extends TaggedNode
---
+++ relay-macro/src/main/scala/relay/graphql.scala
@@ -0,0 +1,3 @@
+package relay
+
+class graphql {}
