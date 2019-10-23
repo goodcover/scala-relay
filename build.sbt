f3b94879c2e1f47f7707bdadcc328f504b0f3b7e
Dan Di Spaltro <dan.dispaltro@gmail.com>
2019-10-23 14:57:16 -0700

    Initial attempt at slinky integration

--- build.sbt
+++ build.sbt
@@ -2,9 +2,12 @@ import sbtrelease.ReleasePlugin.autoImport.{ReleaseStep, _}
 import sbtrelease.ReleaseStateTransformations._
 import scala.sys.process._
 
-val sbtVersions = List("0.13.17", "1.2.8")
+val sbtVersions = List("0.13.17", "1.3.3")
 
 crossSbtVersions := sbtVersions
+
+ThisBuild / intellijPluginName := "relay-slinky-ijext"
+ThisBuild / intellijBuild := "192.6817.14"
 
 lazy val root =
   project
@@ -12,9 +15,10 @@ lazy val root =
     .settings(commonSettings)
     .settings(PgpKeys.publishSigned := {}, publishLocal := {}, publishArtifact := false, publish := {})
     .settings(
-              // crossScalaVersions must be set to Nil on the aggregating project
-              crossScalaVersions := Nil,
-              publish / skip := true)
+      // crossScalaVersions must be set to Nil on the aggregating project
+      crossScalaVersions := Nil,
+      publish / skip := true
+    )
     .aggregate(`sbt-relay-compiler`, `relay-macro`)
 
 def RuntimeLibPlugins = Sonatype && PluginsAccessor.exclude(BintrayPlugin)
@@ -26,75 +30,140 @@ lazy val `sbt-relay-compiler` = project
   .enablePlugins(SbtPlugin)
   .settings(bintraySettings)
   .settings(commonSettings)
-  .settings(sbtPlugin := true,
-            addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % Version.Scalajs),
-            addSbtPlugin("ch.epfl.scala"      % "sbt-scalajs-bundler"      % Version.ScalajsBundler),
-            addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "0.6.1"),
-            scriptedLaunchOpts += "-Dplugin.version=" + version.value,
-            scriptedBufferLog := false,
-////            publishTo := Def.taskDyn {
-////              if (isSnapshot.value) {
-////                // Bintray doesn't support publishing snapshots, publish to Sonatype snapshots instead
-////                Def.task(Option(Opts.resolver.sonatypeSnapshots: Resolver))
-////              } else publishTo.value
-////            },
-            scriptedDependencies := {
-              val () = scriptedDependencies.value
-//              val () = publishLocal.value
-              val () = (publishLocal in `relay-macro`).value
-            },
-            crossSbtVersions := sbtVersions,
-            scalaVersion := {
-              (sbtBinaryVersion in pluginCrossBuild).value match {
-                case "0.13" => "2.10.6"
-                case _      => Version.Scala212
-              }
-            },
-            publishMavenStyle := isSnapshot.value,
-            sourceGenerators in Compile += Def.task {
-              Generators.version(version.value, (sourceManaged in Compile).value)
-            }.taskValue)
+  .settings(
+    sbtPlugin := true,
+    addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % Version.Scalajs),
+    addSbtPlugin("ch.epfl.scala"      % "sbt-scalajs-bundler"      % Version.ScalajsBundler),
+    addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "0.6.1"),
+    scriptedLaunchOpts += "-Dplugin.version=" + version.value,
+    scriptedBufferLog := false,
+    scriptedDependencies := {
+      val () = scriptedDependencies.value
+      val () = (publishLocal in `relay-macro`).value
+    },
+    crossSbtVersions := sbtVersions,
+    scalaVersion := {
+      (sbtBinaryVersion in pluginCrossBuild).value match {
+        case "0.13" => "2.10.6"
+        case _      => Version.Scala212
+      }
+    },
+    publishMavenStyle := isSnapshot.value,
+    sourceGenerators in Compile += Def.task {
+      Generators.version(version.value, (sourceManaged in Compile).value)
+    }.taskValue
+  )
 
 lazy val `relay-macro` = project
   .in(file("relay-macro"))
   .enablePlugins(RuntimeLibPlugins && ScalaJSPlugin)
   .settings(commonSettings)
-  .settings(publishMavenStyle := true,
-            crossSbtVersions := Nil,
-            scalaVersion := Version.Scala212,
-            scalacOptions ++= {
-              if (scalaJSVersion.startsWith("0.6.")) Seq("-P:scalajs:sjsDefinedByDefault")
-              else Nil
-            },
-            publishTo := {
-              val nexus = "https://oss.sonatype.org/"
-              if (isSnapshot.value) Some("snapshots" at nexus + "content/repositories/snapshots")
-              else Some("releases" at nexus + "service/local/staging/deploy/maven2")
-            },
-            crossScalaVersions := Seq(Version.Scala212),
-            addCompilerPlugin("org.scalamacros"         % "paradise" % "2.1.1" cross CrossVersion.full),
-            libraryDependencies ++= Seq(Library.sangria % Provided, Library.scalatest))
+  .settings(
+    publishMavenStyle := true,
+    crossSbtVersions := Nil,
+    scalaVersion := Version.Scala212,
+    scalacOptions ++= {
+      if (scalaJSVersion.startsWith("0.6.")) Seq("-P:scalajs:sjsDefinedByDefault")
+      else Nil
+    },
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (isSnapshot.value) Some("snapshots" at nexus + "content/repositories/snapshots")
+      else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+    crossScalaVersions := Seq(Version.Scala212),
+    addCompilerPlugin("org.scalamacros"         % "paradise" % "2.1.1" cross CrossVersion.full),
+    libraryDependencies ++= Seq(Library.sangria % Provided, Library.scalatest)
+  )
+
+lazy val `slinky-relay` = project
+  .in(file("slinky-relay"))
+  .enablePlugins(ScalaJSPlugin)
+  .settings(commonSettings)
+  .settings(
+    resourceGenerators in Compile += Def.task {
+      val rootFolder = (resourceManaged in Compile).value / "META-INF"
+      rootFolder.mkdirs()
+
+      IO.write(rootFolder / "intellij-compat.json", s"""{
+           |  "artifact": "${organization.value} % slinky-relay-ijext_2.12 % ${version.value}"
+           |}""".stripMargin)
+
+      Seq(rootFolder / "intellij-compat.json")
+    },
+    scalacOptions += "-Xplugin-require:macroparadise",
+    libraryDependencies ++= Vector(
+      "org.scala-lang" % "scala-reflect"    % scalaVersion.value,
+      "org.scala-js"   %% "scalajs-library" % scalaJSVersion
+    ),
+    Library.slinky
+  )
+
+lazy val `slinky-relay-ijext` = (project in file("slinky-relay-ijext"))
+  .enablePlugins(SbtIdeaPlugin)
+  .settings(commonSettings)
+  .settings(
+    intellijPluginName := name.value,
+    intellijExternalPlugins += "org.intellij.scala".toPlugin,
+    intellijInternalPlugins ++= Seq("java"),
+    packageMethod := PackagingMethod.Standalone(), // This only works for proper plugins
+    patchPluginXml := pluginXmlOptions { xml =>
+      // This only works for proper plugins
+      xml.version = version.value
+      xml.sinceBuild = (intellijBuild in ThisBuild).value
+      xml.untilBuild = "193.*"
+    },
+    resourceGenerators in Compile += Def.task {
+      val rootFolder = (resourceManaged in Compile).value / "META-INF"
+      rootFolder.mkdirs()
+      val fileOut = rootFolder / "intellij-compat.xml"
+
+      IO.write(fileOut, s"""
+          |<!DOCTYPE intellij-compat PUBLIC "Plugin/DTD"
+          |        "https://raw.githubusercontent.com/JetBrains/intellij-scala/idea183.x/scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/intellij-compat.dtd">
+          |<intellij-compat>
+          |    <name>Slinky Relay Intellij Support</name>
+          |    <description>Expands Slinky relay macros</description>
+          |    <version>${version.value}</version>
+          |    <vendor>Goodcover</vendor>
+          |    <ideaVersion since-build="2019.2.0" until-build="2019.4.0">
+          |        <extension interface="org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.SyntheticMembersInjector"
+          |                   implementation="slinkyrelay.SlinkyRelayInjector">
+          |            <name>Slinky Relay Intellij Support</name>
+          |            <description>Expansion for @reactRelay macro</description>
+          |        </extension>
+          |    </ideaVersion>
+          |</intellij-compat>
+          """.stripMargin)
+
+      Seq(fileOut)
+    }
+  )
 
 lazy val bintraySettings: Seq[Setting[_]] =
-  Seq(bintrayOrganization := Some("dispalt"),
-      bintrayRepository := "sbt-plugins",
-      bintrayPackage := "sbt-relay-compiler",
-      bintrayReleaseOnPublish := true)
+  Seq(
+    bintrayOrganization := Some("dispalt"), // TODO - Coordinates
+    bintrayRepository := "sbt-plugins",
+    bintrayPackage := "sbt-relay-compiler",
+    bintrayReleaseOnPublish := true
+  )
 
 lazy val commonSettings: Seq[Setting[_]] = Seq(
-  scalacOptions ++= Seq("-feature",
-                        "-deprecation",
-                        "-encoding",
-                        "UTF-8",
-                        "-unchecked",
-                        "-Xlint",
-                        "-Yno-adapted-args",
-                        "-Ywarn-dead-code",
-                        "-Ywarn-numeric-widen",
-                        "-Ywarn-value-discard",
-                        "-Xfuture"),
-  organization := "com.dispalt.relay",
-  sonatypeProfileName := "com.dispalt",
+  scalacOptions ++= Seq(
+    "-feature",
+    "-deprecation",
+    "-encoding",
+    "UTF-8",
+    "-unchecked",
+    "-Xlint",
+    "-Yno-adapted-args",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-Xfuture"
+  ),
+  organization := "com.dispalt.relay",  // TODO - Coordinates
+  sonatypeProfileName := "com.dispalt", // TODO - Coordinates
   pomExtra :=
     <developers>
         <developer>
@@ -103,10 +172,10 @@ lazy val commonSettings: Seq[Setting[_]] = Seq(
           <url>http://dispalt.com</url>
         </developer>
       </developers>,
-  homepage := Some(url(s"https://github.com/dispalt/scala-relay")),
+  homepage := Some(url(s"https://github.com/goodcover/scala-relay")),
   licenses := Seq("MIT License" -> url("http://opensource.org/licenses/mit-license.php")),
   scmInfo := Some(
-    ScmInfo(url("https://github.com/dispalt/scala-relay"), "scm:git:git@github.com:dispalt/scala-relay.git")
+    ScmInfo(url("https://github.com/goodcover/scala-relay"), "scm:git:git@github.com:goodcover/scala-relay.git")
   )
 )
 
@@ -115,20 +184,22 @@ releasePublishArtifactsAction := PgpKeys.publishSigned.value
 releaseCrossBuild := false
 
 releaseProcess :=
-  Seq[ReleaseStep](checkSnapshotDependencies,
-                   inquireVersions,
-                   runClean,
-                   releaseStepCommandAndRemaining("+test"),
-                   setReleaseVersion,
-                   commitReleaseVersion,
-                   tagRelease,
-                   if (sbtPlugin.value) releaseStepCommandAndRemaining("^ publishSigned")
-                   else releaseStepCommandAndRemaining("+ publishSigned"),
-                   releaseStepCommandAndRemaining("sonatypeReleaseAll"),
-                   doReleaseYarn,
-                   setNextVersion,
-                   commitNextVersion,
-                   pushChanges)
+  Seq[ReleaseStep](
+    checkSnapshotDependencies,
+    inquireVersions,
+    runClean,
+    releaseStepCommandAndRemaining("+test"),
+    setReleaseVersion,
+    commitReleaseVersion,
+    tagRelease,
+    if (sbtPlugin.value) releaseStepCommandAndRemaining("^ publishSigned")
+    else releaseStepCommandAndRemaining("+ publishSigned"),
+    releaseStepCommandAndRemaining("sonatypeReleaseAll"),
+    doReleaseYarn,
+    setNextVersion,
+    commitNextVersion,
+    pushChanges
+  )
 
 lazy val doReleaseYarn = { st: State =>
   val v = Project.extract(st)
---
+++ slinky-relay-ijext/src/main/scala/slinkyrelay/SlinkyRelayInjector.scala
@@ -0,0 +1,283 @@
+/**
+  * Modified from slinky.core.SlinkyInjector
+  *
+  * Taken from https://github.com/shadaj/slinky/commit/14095e3345c658ccbd65bc97fdecd21c69d61b71
+  */
+package slinkyrelay
+
+import org.jetbrains.plugins.scala.lang.psi.types.ScParameterizedType
+import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.{SyntheticMembersInjector, TypeDefinitionMembers}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{
+  ScPatternDefinition,
+  ScTypeAliasDeclaration,
+  ScTypeAliasDefinition,
+  ScValueDeclaration
+}
+import org.jetbrains.plugins.scala.lang.psi.types.api.designator.ScDesignatorType
+import org.jetbrains.plugins.scala.lang.psi.types.api.{ParameterizedType, StdType}
+
+class SlinkyRelayInjector extends SyntheticMembersInjector {
+  sealed trait InjectType
+  case object Function extends InjectType
+  case object Type     extends InjectType
+  case object Member   extends InjectType
+
+  override def needsCompanionObject(source: ScTypeDefinition): Boolean = {
+    source.findAnnotationNoAliases("slinkyrelay.annotations.reactRelay") != null
+  }
+
+  def createComponentBody(cls: ScTypeDefinition): Seq[(String, InjectType)] = {
+    val types = TypeDefinitionMembers.getTypes(cls)
+    val (propsDefinition, applyMethods) = types
+      .forName("Props")
+      .iterator
+      .toSeq
+      .headOption
+      .flatMap { elm =>
+        elm.namedElement match {
+          case alias: ScTypeAliasDefinition =>
+            Some(((alias.getText, Member), Seq.empty[(String, InjectType)]))
+          case propsCls: ScClass if propsCls.isCase =>
+            Some(((propsCls.getText, Type), {
+              val paramList        = propsCls.constructor.get.parameterList
+              val caseClassparamss = paramList.params
+              val childrenParam    = caseClassparamss.find(_.name == "children")
+
+              val paramssWithoutChildren = caseClassparamss.filterNot(childrenParam.contains)
+
+              if (childrenParam.isDefined) {
+                if (paramssWithoutChildren.isEmpty) {
+                  Seq(
+                    s"def apply(${childrenParam.get.getText}): slinky.core.KeyAndRefAddingStage[${cls.getQualifiedName}] = ???" -> Function
+                  )
+                } else {
+                  Seq(
+                    s"def apply(${paramssWithoutChildren.map(_.getText).mkString(",")})(${childrenParam.get.getText}): slinky.core.KeyAndRefAddingStage[${cls.getQualifiedName}] = ???" -> Function
+                  )
+                }
+              } else {
+                Seq(
+                  s"def apply(${paramssWithoutChildren.map(_.getText).mkString(",")}): slinky.core.KeyAndRefAddingStage[${cls.getQualifiedName}] = ???" -> Function
+                )
+              }
+            }))
+          case _ => None
+        }
+      }
+      .getOrElse((("", Type), Seq.empty))
+
+    val stateDefinition: Option[(String, InjectType)] = types.forName("State").iterator.toSeq.headOption.flatMap {
+      elm =>
+        elm.namedElement match {
+          case alias: ScTypeAliasDefinition =>
+            Some((alias.getText, Member))
+          case propsCls: ScClass if propsCls.isCase =>
+            Some((propsCls.getText, Type))
+          case _ => None
+        }
+    }
+
+    val snapshotDefinition: Option[(String, InjectType)] = types.forName("Snapshot").iterator.toSeq.headOption.flatMap {
+      elm =>
+        elm.namedElement match {
+          case alias: ScTypeAliasDefinition =>
+            Some((alias.getText, Member))
+          case propsCls: ScClass if propsCls.isCase =>
+            Some((propsCls.getText, Type))
+          case _ => None
+        }
+    }
+
+    (s"type Def = ${cls.getQualifiedName}", Member) +:
+      propsDefinition +:
+      stateDefinition.getOrElse(("type State = Unit", Member)) +:
+      (snapshotDefinition.toList ++
+      applyMethods)
+  }
+
+  def elementAndRefType(external: ScTypeDefinition) = {
+    val superType = external.extendsBlock.superTypes.head
+    val parameters = superType match {
+      case pt: ScParameterizedType =>
+        pt.typeArguments
+      case _ => Seq.empty
+    }
+
+    external.extendsBlock.supers.map(s => s.getQualifiedName).head match {
+      case "slinky.core.ExternalComponent" =>
+        ("Nothing", "scala.scalajs.js.Object")
+      case "slinky.core.ExternalComponentWithAttributes" if parameters.size == 1 =>
+        (parameters.head.canonicalText, "scala.scalajs.js.Object")
+      case "slinky.core.ExternalComponentWithRefType" if parameters.size == 1 =>
+        ("Nothing", parameters.head.canonicalText)
+      case "slinky.core.ExternalComponentWithAttributesWithRefType" if parameters.size == 2 =>
+        (parameters(0).canonicalText, parameters(1).canonicalText)
+      case _ => ("Nothing", "scala.scalajs.js.Object")
+    }
+  }
+
+  def createExternalBody(cls: ScTypeDefinition): Seq[(String, InjectType)] = {
+    val applyMethods = cls.extendsBlock.members
+      .collect {
+        case td: ScTypeDefinition => td
+      }
+      .find(_.name == "Props")
+      .map {
+        case propsCls: ScClass if propsCls.isCase =>
+          val (element, ref) = elementAndRefType(cls)
+          val paramList      = propsCls.constructor.get.parameterList
+
+          if (paramList.params.forall(_.isDefaultParam)) {
+            Seq(
+              s"def apply${paramList.getText}: _root_.slinky.core.BuildingComponent[$element, $ref] = ???"                                                                          -> Function,
+              s"def apply(mod: _root_.slinky.core.AttrPair[$element], tagMods: _root_.slinky.core.AttrPair[$element]*): _root_.slinky.core.BuildingComponent[$element, $ref] = ???" -> Function,
+              s"def withKey(key: String): _root_.slinky.core.BuildingComponent[$element, $ref] = ???"                                                                               -> Function,
+              s"def withRef(ref: $ref => Unit): _root_.slinky.core.BuildingComponent[$element, $ref] = ???"                                                                         -> Function,
+              s"def withRef(ref: _root_.slinky.core.facade.ReactRef[$ref]): _root_.slinky.core.BuildingComponent[$element, $ref] = ???"                                             -> Function,
+              s"def apply(children: _root_.slinky.core.facade.ReactElement*): _root_.slinky.core.BuildingComponent[$element, $ref] = ???"                                           -> Function
+            )
+          } else {
+            Seq(s"def apply${paramList.getText}: slinky.core.BuildingComponent[$element, $ref] = ???" -> Function)
+          }
+
+        case _ => Seq.empty
+      }
+      .getOrElse(Seq.empty)
+
+    applyMethods
+  }
+
+  def createFunctionalComponentBody(cls: ScTypeDefinition): Seq[(String, InjectType)] = {
+    val applyMethods = cls.extendsBlock.members
+      .collect {
+        case td: ScClass               => td
+        case td: ScTypeAliasDefinition => td
+      }
+      .find(_.name == "Props")
+      .flatMap { elm =>
+        elm match {
+          case alias: ScTypeAliasDefinition =>
+            Some(Seq(s"def apply(props: ${cls.name}.Props): ${cls.name}.component.Result = ???" -> Function))
+          case propsCls: ScClass if propsCls.isCase =>
+            Some {
+              val paramList        = propsCls.constructor.get.parameterList
+              val caseClassparamss = paramList.params
+              val childrenParam    = caseClassparamss.find(_.name == "children")
+
+              val paramssWithoutChildren = caseClassparamss.filterNot(childrenParam.contains)
+
+              if (childrenParam.isDefined) {
+                if (paramssWithoutChildren.isEmpty) {
+                  Seq(s"def apply(${childrenParam.get.getText}): ${cls.name}.component.Result = ???" -> Function)
+                } else {
+                  Seq(
+                    s"def apply(${paramssWithoutChildren.map(_.getText).mkString(",")})(${childrenParam.get.getText}): ${cls.name}.component.Result = ???" -> Function
+                  )
+                }
+              } else {
+                Seq(
+                  s"def apply(${paramssWithoutChildren.map(_.getText).mkString(",")}): ${cls.name}.component.Result = ???" -> Function
+                )
+              }
+            }
+          case _ => None
+        }
+      }
+      .getOrElse(Seq.empty)
+
+    applyMethods
+  }
+
+  def isSlinky(tpe: ScTypeDefinition): Boolean = {
+    tpe.findAnnotationNoAliases("slinkyrelay.annotations.reactRelay") != null
+  }
+
+  def isExternal(tpe: ScTypeDefinition): Boolean = {
+    isSlinky(tpe) && tpe.extendsBlock.supers.map(_.getQualifiedName).exists { parent =>
+      parent == "slinky.core.ExternalComponent" ||
+      parent == "slinky.core.ExternalComponentWithAttributes" ||
+      parent == "slinky.core.ExternalComponentWithRefType" ||
+      parent == "slinky.core.ExternalComponentWithAttributesWithRefType"
+    }
+  }
+
+  def isComponent(tpe: ScTypeDefinition): Boolean = {
+    isSlinky(tpe) && tpe.extendsBlock.supers.map(_.getQualifiedName).exists { parent =>
+      parent == "slinky.core.Component" || parent == "slinky.core.StatelessComponent"
+    }
+  }
+
+  def isFunctionalComponent(tpe: ScTypeDefinition): Boolean = {
+    isSlinky(tpe) && tpe.extendsBlock.members.exists {
+      case td: ScValueDeclaration if td.declaredNames == Seq("component") => true
+      case pd: ScPatternDefinition =>
+        pd.bindings.exists(_.getName == "component")
+      case _ => false
+    }
+  }
+
+  override def injectFunctions(source: ScTypeDefinition): Seq[String] = {
+    (source match {
+      case obj: ScObject if isExternal(obj) =>
+        createExternalBody(obj)
+
+      case obj: ScObject if isFunctionalComponent(obj) =>
+        createFunctionalComponentBody(obj)
+
+      case obj: ScObject =>
+        obj.fakeCompanionClassOrCompanionClass match {
+          case clazz: ScTypeDefinition if isComponent(clazz) =>
+            createComponentBody(clazz)
+          case _ => Seq.empty
+        }
+
+      case _ => Seq.empty
+    }).filter(_._2 == Function).map(_._1)
+  }
+
+  override def injectInners(source: ScTypeDefinition): Seq[String] = {
+    (source match {
+      case obj: ScObject =>
+        obj.fakeCompanionClassOrCompanionClass match {
+          case clazz: ScTypeDefinition if isComponent(clazz) =>
+            createComponentBody(clazz)
+          case _ => Seq.empty
+        }
+
+      case _ => Seq.empty
+    }).filter(_._2 == Type).map(_._1)
+  }
+
+  override def injectMembers(source: ScTypeDefinition): Seq[String] = {
+    (source match {
+      case obj: ScObject if isExternal(obj) =>
+        createExternalBody(obj)
+
+      case obj: ScObject if isFunctionalComponent(obj) =>
+        createFunctionalComponentBody(obj)
+
+      case obj: ScObject =>
+        obj.fakeCompanionClassOrCompanionClass match {
+          case clazz: ScTypeDefinition if isComponent(clazz) =>
+            createComponentBody(clazz)
+          case _ => Seq.empty
+        }
+
+      case _ => Seq.empty
+    }).filter(_._2 == Member).map(_._1)
+  }
+
+  override def injectSupers(source: ScTypeDefinition): Seq[String] = {
+    source match {
+      case obj: ScObject =>
+        obj.fakeCompanionClassOrCompanionClass match {
+          case clazz: ScTypeDefinition if isComponent(clazz) =>
+            Seq("slinky.core.ComponentWrapper")
+          case _ => Seq.empty
+        }
+
+      case _ => Seq.empty
+    }
+  }
+}
