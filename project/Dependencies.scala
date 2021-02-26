792b215319560cf5398dd0b56f766ff750be9ea3
Scala Steward <me@scala-steward.org>
2021-02-26 23:37:41 +0100

    Update scala-library, scala-reflect to 2.12.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Version {
   final val Scala211 = "2.11.11"
-  final val Scala212 = "2.12.10"
+  final val Scala212 = "2.12.13"
   final val Scala213 = "2.13.2"
 
   final val Slinky         = "0.6.6"
