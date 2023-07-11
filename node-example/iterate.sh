219ff0c87190aabbe1a2872b02b21e34beec499d
Jason Pickens <jasonpickensnz@gmail.com>
2023-07-11 20:39:28 +1200

    Add IntelliJ run configuration

--- node-example/iterate.sh
+++ node-example/iterate.sh
@@ -5,4 +5,4 @@ set -xe
 pushd ../node-compiler && yarn build && yarn link
 popd
 yarn link relay-compiler-language-scalajs
-node --inspect-brk ./node_modules/.bin/relay-compiler --language scalajs --schema ./schema.graphql --src src/ --artifactDirectory target/ --customScalars.Protobuf=com.pack.Class
+node ./node_modules/.bin/relay-compiler --language scalajs --schema ./schema.graphql --src src/ --artifactDirectory target/ --customScalars.Protobuf=com.pack.Class
