43e40ade884f723de7be75098537afc5a33dd8c9
Jason Pickens <jasonpickensnz@gmail.com>
2023-07-11 20:20:25 +1200

    Use yarn link instead of add

--- node-example/iterate.sh
+++ node-example/iterate.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-pushd ../node-compiler && yarn build
+pushd ../node-compiler && yarn build && yarn link
 popd
-yarn add file:../node-compiler
-node ./node_modules/.bin/relay-compiler --language scalajs --schema ./schema.graphql --src src/ --artifactDirectory target/ --customScalars.Protobuf=com.pack.Class
+yarn link relay-compiler-language-scalajs
+node --inspect-brk ./node_modules/.bin/relay-compiler --language scalajs --schema ./schema.graphql --src src/ --artifactDirectory target/ --customScalars.Protobuf=com.pack.Class
