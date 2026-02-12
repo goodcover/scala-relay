c649641c7bc5aa44842e8e84d36b58f9afbbbada
Dan Di Spaltro <dan.dispaltro@gmail.com>
2026-02-12 12:22:22 -0800

    Update java version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,13 @@ import sbt.Keys._
 import sbt._
 
 object Versions {
+  // Update the .github/workflows/scala.yml with matching Scala versions
   final val Scala212 = "2.12.21"
+  // Update the .github/workflows/scala.yml with matching Scala versions
   final val Scala213 = "2.13.18"
   final val Scala3   = "3.3.7"
-  final val Scala37  = "3.7.4"
+  // Update the .github/workflows/scala.yml with matching Scala versions
+  final val Scala38  = "3.8.1"
 
   final val Mill = "1.1.2"
 
---
+++ scala-relay-core/src/main/scala-2.12/com/goodcover/relay/HookImplicits.scala
@@ -0,0 +1,6 @@
+package com.goodcover.relay
+
+trait HookImplicits {
+
+  type Id[A] = A
+}
