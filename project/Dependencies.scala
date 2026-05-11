2a4b4f7384a5c02d5e69d79b264d0cefc8db5c7e
Scala Steward <scala_steward@virtuslab.com>
2026-05-11 19:32:08 +0000

    Update scalameta to 4.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   final val Scalajs = org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.scalaJSVersion
 
   final val Caliban   = "3.1.0"
-  final val ScalaMeta = "4.15.2"
+  final val ScalaMeta = "4.17.0"
   final val UTest     = "0.9.5"
 }
 
